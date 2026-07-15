a8aeebcc4fed36ffb206a211daddad3730e658f1
foadtmoreno-cloud <foadtmoreno@gmail.com>
2026-07-15 13:01:39 -0600

    Add files via upload

--- PAGINA WEB WILSON/config.js
+++ PAGINA WEB WILSON/config.js
@@ -5,5 +5,6 @@
 const SUPABASE_URL = 'https://nkhkqicoqcizpoqhjsph.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5raGtxaWNvcWNpenBvcWhqc3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMjk3NTEsImV4cCI6MjA5OTcwNTc1MX0.jREXHYKubZNmT83oQ0Eoo94gUwTLyatNYAWWe9kdM9E';
 
-// No modificar nada debajo de esta línea
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Crear cliente Supabase global
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
